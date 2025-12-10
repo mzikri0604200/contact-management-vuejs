855c381750e529f82e0dc96b0b76a32216e5dec3
mzikri0604200 <muhzikri06@gmail.com>
2025-12-10 11:23:17 +0700

    Feat : Add Dashboard Layout and add Router Path in main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Layout from './components/Layout.vue'
 import UserRegister from './components/user/UserRegister.vue'
 import UserLogin from './components/user/UserLogin.vue'
+import DashboardLayout from './components/DashboardLayout.vue'
+import ContactList from './components/contact/ContactList.vue'
+import UserProfile from './components/user/UserProfile.vue'
 
 const router = createRouter({
 	history: createWebHistory(),
@@ -22,6 +25,21 @@ const router = createRouter({
 					component: UserLogin
 				},
 			]
+		},
+		{
+			component: DashboardLayout,
+			path: '/dashboard',
+			children: [
+				{
+					path: 'contacts',
+					component: ContactList
+				},
+				{
+					path: 'users/profile',
+					component: UserProfile
+				}
+			]
+			
 		}
 	]
 	
