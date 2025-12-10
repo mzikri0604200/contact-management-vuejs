753ed59b5a20bced1d5d1358d7e07838a14c1f37
mzikri0604200 <muhzikri06@gmail.com>
2025-12-10 11:24:53 +0700

    Feat : Add Fitur - Page Profile - Update Name dan Password - Update UserAPI

--- src/lib/api/UserApi.js
+++ src/lib/api/UserApi.js
@@ -1,7 +1,7 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const userRegister = async ({ username, name, password }) => {
-	return await fetch(`${apiUrl}/api/users`, {
+	return await fetch(`${apiUrl}/users`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -12,7 +12,7 @@ export const userRegister = async ({ username, name, password }) => {
 }
 
 export const userLogin = async ({ username, password }) => {
-	return await fetch(`${apiUrl}/api/users/login`, {
+	return await fetch(`${apiUrl}/users/login`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -21,3 +21,37 @@ export const userLogin = async ({ username, password }) => {
 		body: JSON.stringify({ username, password })
 	})
 }
+
+export const userDetail = async (token) => {
+	return await fetch(`${apiUrl}/users/current`, {
+		method: 'GET',
+		headers: {
+			'Accept': 'application/json',
+			'Authorization':token
+		}
+	})
+}
+
+export const userUpdateProfile = async (token, { name}) => {
+  return await fetch(`${apiUrl}/users/current`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({ name }),
+  });
+};
+
+export const userUpdatePassword = async (token, { password}) => {
+  return await fetch(`${apiUrl}/users/current`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({ password }),
+  });
+};
