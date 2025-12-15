7f946b6b4eb88157af7ab436b507c198bae9376f
mzikri0604200 <muhzikri06@gmail.com>
2025-12-15 14:32:56 +0700

    Setup TailwindCSS dengan benar tidak menggunakan CDN

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom': '0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      },
+    },
+  },
+  plugins: [],
+}
+
